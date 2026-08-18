7cf0c25583f0f17903fe88b40d15403b22313be9
johnnyn <johnnyxn@outlook.com>
2026-08-17 19:29:09 -0700

    added basic hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+
+export default function Hero() {
+    return (
+        <section>
+            <h1>Hi, my name is Johnny Nguyen.</h1>
+            <p>An aspiring software engineer interested in web development.
+                I thrive in collaborative environments and love to overcome new challenges.
+            </p>
+            <button>Github</button>
+            <button>LinkedIn</button>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "@/app/components/Navbar"
 import Footer from "@/app/components/Footer"
+import Hero from "@/app/components/Hero"
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-     <h1>Home Page</h1>
+      <Hero/>
       <Footer/>
     </div>
   );

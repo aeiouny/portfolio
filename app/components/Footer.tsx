9f8ba4b5786d25e2229adc2d2d835db2cc3a53e4
johnnyn <johnnyxn@outlook.com>
2026-08-17 19:17:07 -0700

    added basic navbar and footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>2026. Johnny Nguyen. All rights reserved.</p>
+        </footer>
+    )
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Navbar() {
+  return (
+    <header>
+        <nav>
+            <a>About</a>
+            <a>Experience</a>
+            <a>Projects</a>
+            <a>Technologies</a>
+            <a>Contact</a>
+            <a>Resume</a>
+        </nav>
+    </header>
+  );
+}

c841b369e25057633fc81ccbc6d0abafd3f96ee0
johnnyn <johnnyxn@outlook.com>
2026-08-19 22:28:19 -0700

    added a simple contact section

---
+++ app/components/Contact.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Contact() {
+    return (
+        <section>
+            <h2>Get In Touch</h2>
+            <p>
+                Currently looking for new opportunities. Feel free to reach out, or
+                just say hi! 
+            </p>
+            <a>johnnyxn@outlook.com</a>
+            <a>GitHub</a>
+            <a>LinkedIn</a>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/app/components/Navbar"
 import Footer from "@/app/components/Footer"
 import Hero from "@/app/components/Hero"
+import Contact from "@/app/components/Contact"
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Contact/>
       <Footer/>
     </div>
   );

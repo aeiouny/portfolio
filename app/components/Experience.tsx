e66139232619103b50c6425d8317db82103819d7
johnnyn <johnnyxn@outlook.com>
2026-08-26 22:10:35 -0700

    starter experience templates

---
+++ app/components/Experience.tsx
@@ -0,0 +1,17 @@
+
+interface ExperienceItem {
+    role: string;
+    company: string;
+    period: string;
+    descripton: string[];
+}
+
+
+export default function Experience() {
+    return (
+        <section>
+            <h2>Experience</h2>
+
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Navbar from "@/app/components/Navbar"
 import Footer from "@/app/components/Footer"
 import Hero from "@/app/components/Hero"
 import Contact from "@/app/components/Contact"
+import Experience from "@/app/components/Experience"
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Experience/>
       <Contact/>
       <Footer/>
     </div>
